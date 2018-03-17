82fd80ab943f38404ca4ef1063427f36fca9cace
Alexander Forselius <alexander.forselius@buddhalow.com>
2018-03-17 20:03:06 +0100

    Changing from handlebars to ejs due to css problems

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const CURR_DIR = process.cwd();
 const inquirer = require('inquirer');
 const fs = require('fs');
 const handlebars = require('handlebars')
-
+const mkpath = require('mkpath')
 var program = require('commander');
-
+var ejs = require('ejs')
+const fse = require('fs-extra');
 program
   .version('0.2.8')
   .command('init [projectName] [schemaFilePath] [template]')
@@ -24,40 +25,57 @@ program
     }
     const templatePath = `${__dirname}/templates/${templateName}`;
     
-    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+    fs.mkdirSync(`${projectName}`);
     
-    createDirectoryContents(templatePath, projectName, schemaFilePath);
+    let schema = JSON.parse(fs.readFileSync(schemaFilePath))
+
+    createDirectoryContents(templatePath, projectName, schema);
   });
 
 program.parse(process.argv);
 
 
-function createDirectoryContents (templatePath, newProjectPath, schemaFileName) {
+function createDirectoryContents (templatePath, newProjectPath, schema) {
   const filesToCreate = fs.readdirSync(templatePath);
-    
-  let schema = fs.readFileSync(schemaFileName, 'utf8')
-
+  console.log(schema)
   filesToCreate.forEach(file => {
+    console.log(file)
+    if (file == 'node_modules') return
+    if (file.indexOf('.') == 0) return
     const origFilePath = `${templatePath}/${file}`;
     
     // get stats about the current file
     const stats = fs.statSync(origFilePath);
-
+    
     if (stats.isFile()) {
+    
       let contents = fs.readFileSync(origFilePath, 'utf8');
-
-      const handleBarsExtension = '.handlebars'
+      const handleBarsExtension = '.ejs'
       let newFile = file
       // If the file has a handlebars extension, generate a common file instead
-      if (origFilePath.indexOf(handlebars) == origFilePath.length - handleBarsExtension.length) {
+      if (origFilePath.indexOf(handleBarsExtension) == origFilePath.length - handleBarsExtension.length) {
        newFile = origFilePath.substr(0, origFilePath.length - handleBarsExtension.length)
-       let template = handlebars.compile(contents)
-       contents = template(schema)
+       if (newFile.indexOf('.model') > 0) {
+        for (let model of schema.models) {
+          
+          newFile = newFile.replace('.model', '.' + model.singular) 
+        
+          let contents = ejs.render(contents, model)
+
+          const writePath = `${CURR_DIR}/${newProjectPath}/${newFile}`;
+        
+          fse.outputFileSync(writePath, contents);
+
+        }
+        return
+       }
+       contents = template(contents, schema)
       }
       
-      const writePath = `${CURR_DIR}/${newProjectPath}/${newFile}`;
-      fs.writeFileSync(writePath, contents, 'utf8');
+      const writePath = `${newProjectPath}/${newFile}`;
+      fse.outputFileSync(writePath, contents);
     } else {
+      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
       createDirectoryContents(origFilePath, writePath, schema)
     }
   });
